293526a517a754d6f26b52fef4ff04ef5b0d79cc
GordeevGleb <excexcexc@yandex.com>
2024-07-19 13:36:18 +0300

    17.2:1

--- main-service/src/main/java/ru.practicum/repository/RequestRepository.java
+++ main-service/src/main/java/ru.practicum/repository/RequestRepository.java
@@ -1,8 +1,6 @@
 package ru.practicum.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import ru.practicum.entity.Request;
 import ru.practicum.enums.RequestStatus;
--- main-service/src/main/java/ru.practicum/service/request/RequestServiceImpl.java
+++ main-service/src/main/java/ru.practicum/service/request/RequestServiceImpl.java
@@ -19,7 +19,6 @@ import ru.practicum.repository.UserRepository;
 
 import java.time.LocalDateTime;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Service
 @Slf4j
