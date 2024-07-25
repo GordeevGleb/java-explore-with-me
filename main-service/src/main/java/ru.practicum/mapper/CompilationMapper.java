ef3fef8f9c2d3f6a5afb7c67fd1c908780abe3ae
GordeevGleb <excexcexc@yandex.com>
2024-07-25 10:20:24 +0300

    17.2:2

--- main-service/src/main/java/ru.practicum/mapper/CompilationMapper.java
+++ main-service/src/main/java/ru.practicum/mapper/CompilationMapper.java
@@ -7,16 +7,16 @@ import ru.practicum.dto.compilation.NewCompilationDto;
 import ru.practicum.dto.event.EventShortDto;
 import ru.practicum.entity.Compilation;
 import ru.practicum.entity.Event;
-
-import java.util.Collection;
 import java.util.HashSet;
 import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface CompilationMapper {
+
     @Mapping(target = "events", source = "events")
     CompilationDto toCompilationDto(Compilation compilation, List<EventShortDto> events);
-@Mapping(target = "events", source = "events")
+
+    @Mapping(target = "events", source = "events")
     List<CompilationDto> toListCompilationDto(List<Compilation> compilations);
 
     @Mapping(target = "events", source = "events")
--- main-service/src/main/java/ru.practicum/repository/EventRepository.java
+++ main-service/src/main/java/ru.practicum/repository/EventRepository.java
@@ -4,10 +4,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
-import ru.practicum.dto.event.EventShortDto;
-import ru.practicum.entity.Compilation;
 import ru.practicum.entity.Event;
 
 import java.util.List;
