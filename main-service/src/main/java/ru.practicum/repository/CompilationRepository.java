fd6278abb1aee1327c4b38a292b1af1ad270fbe8
GordeevGleb <excexcexc@yandex.com>
2024-07-26 07:43:15 +0300

    17.2:3

--- main-service/src/main/java/ru.practicum/repository/CompilationRepository.java
+++ main-service/src/main/java/ru.practicum/repository/CompilationRepository.java
@@ -7,7 +7,6 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import ru.practicum.entity.Compilation;
 
-import java.util.List;
 
 @Repository
 public interface CompilationRepository extends JpaRepository<Compilation, Long> {
