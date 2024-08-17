08104d620b2c07ef0633662e4f5a09b031975561
GordeevGleb <excexcexc@yandex.com>
2024-08-17 22:51:14 +0300

    17.2:3

--- main-service/src/main/java/ru.practicum/service/request/RequestServiceImpl.java
+++ main-service/src/main/java/ru.practicum/service/request/RequestServiceImpl.java
@@ -59,8 +59,8 @@ public class RequestServiceImpl implements RequestService {
                 .status(RequestStatus.PENDING)
                 .build();
         if (event.getRequestModeration().equals(Boolean.FALSE) || event.getParticipantLimit() == 0) {
-            event.setConfirmedRequestCount(Optional.ofNullable
-                    (event.getConfirmedRequestCount()).isEmpty() ? 1L : event.getConfirmedRequestCount() + 1);
+            event.setConfirmedRequestCount(Optional.ofNullable(
+                    event.getConfirmedRequestCount()).isEmpty() ? 1L : event.getConfirmedRequestCount() + 1);
             actual.setStatus(RequestStatus.CONFIRMED);
         }
         requestRepository.save(actual);
