43d450249509501a950a70b9665a4837e837043f
GordeevGleb <excexcexc@yandex.com>
2024-09-12 14:09:02 +0300

    17.3:2

--- main-service/src/main/java/ru.practicum/repository/RatingRepository.java
+++ main-service/src/main/java/ru.practicum/repository/RatingRepository.java
@@ -3,7 +3,6 @@ package ru.practicum.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import ru.practicum.entity.Rating;
 
 import java.util.List;
